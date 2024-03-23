1447114a41f7f32b239aca2cc2e627f90f3aacfd
root <j.nformi1@alustudent.com>
2024-03-23 06:44:20 -0700

    Tasks

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" a Python script that fetches https://intranet.hbtn.io/status """
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
 
 
-if __name__ == '__main__':
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
+if __name__ == "__main__":
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
         print("Body response:")
-        html = response.read()
-        print("\t- type:", type(html))
-        print("\t- content:", html)
-        print("\t- utf8 content:", html.decode('utf-8'))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
